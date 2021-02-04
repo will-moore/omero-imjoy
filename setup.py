87d597aa80a5d05686e111cba63b130ee76b8c6a
William Moore <w.moore@dundee.ac.uk>
2021-02-04 14:02:15 +0000

    Initial commit with test.jpg

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='omero-imjoy',
+    version='0.0.1',
+    description="OMERO.web plugin for ImJoy",
+    packages=find_packages(),
+    keywords=['omero', 'imjoy'],
+)
