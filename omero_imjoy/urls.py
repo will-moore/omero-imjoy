0e66d04d582704c4f8290522a376d80eb5734d2e
William Moore <w.moore@dundee.ac.uk>
2021-03-12 16:00:40 +0000

    Use zarr to load 8bit image for ImageJ.JS

--- omero_imjoy/urls.py
+++ omero_imjoy/urls.py
@@ -22,5 +22,18 @@ from . import views
 urlpatterns = [
 
     # index 'home page' of the app
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='imjoy_index'),
+    url(r'^test/(?P<iid>[0-9]+)/$', views.test, name='imjoy_test'),
+
+    url(r'^save_image/$', views.save_image, name='imjoy_save_image'),
+
+    url(r'^zarr/(?P<iid>[0-9]+)/.zattrs$',
+        views.zarr_zattrs, name='imjoy_zarr_zattrs'),
+
+    url(r'^zarr/(?P<iid>[0-9]+)/(?P<level>[0-9]+)/.zarray$',
+        views.zarr_zarray, name='imjoy_zarr_zarray'),
+
+    url(r'^zarr/(?P<iid>[0-9]+)/(?P<level>[0-9]+)/(?P<t>[0-9]+).(?P<c>[0-9]+).(?P<z>[0-9]+).(?P<y>[0-9]+).(?P<x>[0-9]+)$',
+        views.zarr_chunk, name='imjoy_zarr'),
+
 ]
--- omero_imjoy/views.py
+++ omero_imjoy/views.py
@@ -15,13 +15,23 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
 from django.core.urlresolvers import reverse
 from django.template import loader
 from django.templatetags import static
 from django.shortcuts import render
+from wsgiref.util import FileWrapper
+import json
+import zarr
+from PIL import Image
+from io import BytesIO
+import base64
+import numpy as np
+import tempfile
+import os
 
 from omeroweb.webclient.decorators import login_required
+from omeroweb.webgateway.marshal import channelMarshal
 
 # @login_required()
 def index(request, conn=None, **kwargs):
@@ -29,3 +39,109 @@ def index(request, conn=None, **kwargs):
     OMERO ImJoy Home page 
     """
     return render(request, 'omero_imjoy/index.html', {})
+
+
+def test(request, iid):
+    print('test', iid)
+    return render(request, 'omero_imjoy/zarr_test.html', {'iid': iid})
+
+
+@login_required()
+def zarr_zattrs(request, iid, conn=None, **kwargs):
+
+    image = conn.getObject("Image", iid)
+
+    rv = {
+        "multiscales": [
+            {
+                "datasets": [
+                    {
+                        "path": "0"
+                    }
+                ],
+                "version": "0.1"
+            }
+        ],
+        "omero": {
+            "channels": [channelMarshal(x) for x in image.getChannels()],
+            "id": image.id,
+            "rdefs": {
+                "defaultT": image._re.getDefaultT(),
+                "defaultZ": image._re.getDefaultZ(),
+                "model": (image.isGreyscaleRenderingModel() and "greyscale" or "color"),
+            }
+        }
+    }
+    return JsonResponse(rv)
+
+
+@login_required()
+def zarr_zarray(request, iid, level, conn=None, **kwargs):
+
+    image = conn.getObject("Image", iid)
+    shape = [getattr(image, 'getSize' + dim)() for dim in ('TCZYX')]
+    print('shape', shape)
+    chunks = (1, 1, 1, shape[3], shape[4])
+    rsp = {"data": "fail"}
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # writes zarray
+        zarr.open_array(tmpdirname, mode='w', shape=tuple(shape), chunks=chunks)
+
+        # reads zarray
+        zattrs_path = os.path.join(tmpdirname, '.zarray')
+        with open(zattrs_path, 'r') as reader:
+            json_text = reader.read()
+            rsp = json.loads(json_text)
+
+    return JsonResponse(rsp)
+
+
+@login_required()
+def zarr_chunk(request, iid, level, t, c, z, y, x, conn=None, **kwargs):
+
+    x, y, z, c, t = [int(dim) for dim in (x, y, z, c, t)]
+
+    # NB: Assume that x and y are 0 and we are loading whole z, c, t plane
+    image = conn.getObject("Image", iid)
+    shape = [getattr(image, 'getSize' + dim)() for dim in ('TCZYX')]
+    chunks = (1, 1, 1, shape[3], shape[4])
+
+    plane = image.getPrimaryPixels().getPlane(z, c, t)
+    data = ""
+    chunk_name = ".".join([str(dim) for dim in [t, c, z, y, x]])
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # write chunk
+        zarr_array = zarr.open_array(tmpdirname, mode='w', shape=tuple(shape), chunks=chunks)
+        zarr_array[t, c, z, :, :] = plane
+
+        # reads zarray
+        chunk_path = os.path.join(tmpdirname, chunk_name)
+        with open(chunk_path, 'rb') as reader:
+            data = reader.read()
+
+    rsp = HttpResponse(data)
+    rsp["Content-Length"] = len(data)
+    rsp["Content-Disposition"] = "attachment; filename=%s" % chunk_name
+    return rsp
+
+
+@login_required(setGroupContext=True)
+def save_image(request, conn=None, **kwargs):
+
+    json_data = json.loads(request.body)
+
+    img_data64 = json_data['data'].replace('data:image/png;base64,', '')
+    im = Image.open(BytesIO(base64.b64decode(img_data64)))
+    im.show()
+
+    np_array = np.asarray(im)
+    red = np_array[::, ::, 0]
+    green = np_array[::, ::, 1]
+    blue = np_array[::, ::, 2]
+    plane_gen = iter([red, green, blue])
+    new_image = conn.createImageFromNumpySeq(
+        plane_gen,
+        "Plot from OMERO.parade",
+        sizeC=3)
+
+    return JsonResponse({'image_id': new_image.id})
